06101c0ea656f1622924985b941c50adc6227082
Ilya Vikharev <vikharev38@gmail.com>
2025-04-30 10:42:07 +0300

    Решение задач 6-го дня

--- app/obj/Debug/net9.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511ae1df77d468dcc2d31531ba519185d19a7105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c57f0528f2bc75eb1699cf619ae7b59f74d95c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
